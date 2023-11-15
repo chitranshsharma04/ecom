66e4db0b73fec8e499017881e68b239db976b197
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-15 18:06:57 +0530

    Update index.jsx

--- components/AddressBook/index.jsx
+++ components/AddressBook/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import Link from 'next/link';
 import {toast} from 'react-toastify';
 import {useContextState} from '@context/reducer';
