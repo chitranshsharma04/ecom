98d8ed147b9d7bd42901ae14e2bfd764e929fdc1
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-15 16:46:05 +0530

    Update ManageReturns.js

--- components/MultiVendor/ManageReturns/ManageReturns.js
+++ components/MultiVendor/ManageReturns/ManageReturns.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import Link from 'next/link';
 import moment from 'moment';
 import AccountSidebar from '@components/Common/Account/AccountSidebar';
