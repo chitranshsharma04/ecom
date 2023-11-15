5bac844d438b0ac97a3f6bc7e8be4972ff303721
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-15 16:45:34 +0530

    Update ReportManager.jsx

--- components/MultiVendor/ReportManager/ReportManager.jsx
+++ components/MultiVendor/ReportManager/ReportManager.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import {useState, useCallback, useEffect} from 'react';
 import AccountSidebar from '@components/Common/Account/AccountSidebar';
 import SpinnerLoader from '@components/Common/SpinnerLoader/SpinnerLoader';
 import {api} from '@utils/api';
