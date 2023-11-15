fef268921e85096edd00c6eac1e2d9b21f882d53
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-15 18:13:47 +0530

    Update AccountSidebar.jsx

--- components/Common/Account/AccountSidebar.jsx
+++ components/Common/Account/AccountSidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import Link from 'next/link';
 import cookie from 'js-cookie';
 import {useRouter} from 'next/router';
