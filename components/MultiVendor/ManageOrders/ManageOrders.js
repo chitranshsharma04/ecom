1f1148abaa4c7683bd1abe15d2bd52084f259f6a
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-15 16:46:29 +0530

    Update ManageOrders.js

--- components/MultiVendor/ManageOrders/ManageOrders.js
+++ components/MultiVendor/ManageOrders/ManageOrders.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import Link from 'next/link';
 import AccountSidebar from '@components/Common/Account/AccountSidebar';
 import SpinnerLoader from '@components/Common/SpinnerLoader/SpinnerLoader';
