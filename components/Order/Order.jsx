9046a2174e731fbfdff6e2159ebdfeb1c0361902
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-16 12:14:50 +0530

    Update Order.jsx

--- components/Order/Order.jsx
+++ components/Order/Order.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState, useEffect} from 'react';
+import {useCallback, useState, useEffect} from 'react';
 import Link from 'next/link';
 import {toast} from 'react-toastify';
 import AccountSidebar from '@components/Common/Account/AccountSidebar';
