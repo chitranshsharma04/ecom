283399b7fa5904a2d07c611318cf0992f9eec022
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-16 14:18:39 +0530

    Update TermsAndConditions.js

--- components/TermsAndConditions/TermsAndConditions.js
+++ components/TermsAndConditions/TermsAndConditions.js
@@ -19,10 +19,10 @@ const TermsAndConditions = () => {
 							looking at its layout. The point of using Lorem
 							Ipsum is that it has a more-or-less normal
 							distribution of letters, as opposed to using
-							'Content here, content here', making it look like
+							&apos;Content here, content here&apos;, making it look like
 							readable English. Many desktop publishing packages
 							and web page editors now use Lorem Ipsum as their
-							default model text, and a search for 'lorem ipsum'
+							default model text, and a search for &apos;lorem ipsum&apos;
 							will uncover many web sites still in their infancy.
 							Various versions have evolved over the years,
 							sometimes by accident, sometimes on purpose
