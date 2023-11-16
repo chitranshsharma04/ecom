8e6a47cc58a085af94dd42ab23249e263d4a061c
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-16 12:08:32 +0530

    Update [slug].js

--- pages/orderDetail/[slug].js
+++ pages/orderDetail/[slug].js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import React from 'react';
+// import React from 'react';
 
 import OrderDetail from '@components/Order/OrderDetail';
 import {api} from '@utils/api';
