672dbd8ca359e9e1261d69f2c4bd29dbed2079fe
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-17 16:30:43 +0530

    Update Image.jsx

--- components/Common/Image.jsx
+++ components/Common/Image.jsx
@@ -7,7 +7,7 @@ function NextImage({src, alt, ...rest}) {
 	return (
 		<img
 			// src={imageError ? '/assets/images/missingImage.png' : src}
-			src={src || '/assets/images/missingImage.png'}
+			src={src ? src : '/assets/images/missingImage.png'}
 			alt={alt?.toString() ?? 'demo'}
 			// priority
 			// onError={() => setImageError(true)}
