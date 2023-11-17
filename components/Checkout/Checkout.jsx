3ff5e54bd197dc6dc0bdac741870ec486069f78d
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-17 14:35:01 +0530

    Update Checkout.jsx

--- components/Checkout/Checkout.jsx
+++ components/Checkout/Checkout.jsx
@@ -352,7 +352,7 @@ const Checkout = () => {
 				};
 
 				if (coupon) {
-					data['promocode'] = coupon.code;
+					data.promocode = coupon.code;
 				}
 
 				if (state?.billingAddress === false) {
