bea364f94f3587dba418a5ec4879e33f2499a620
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-17 14:34:47 +0530

    Update Checkout.jsx

--- components/Checkout/Checkout.jsx
+++ components/Checkout/Checkout.jsx
@@ -365,7 +365,7 @@ const Checkout = () => {
 						country: state.billing_country,
 					};
 
-					data['billing_address'] = billAddr;
+					data.billing_address = billAddr;
 				} else {
 					const billAddr = {
 						address: state.shipping_address1,
