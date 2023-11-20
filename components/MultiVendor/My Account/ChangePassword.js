fa4f4c2cb89a9dce22628d4e8643519df9c3b26e
chitranshsharma04 <148422097+chitranshsharma04@users.noreply.github.com>
2023-11-20 11:34:07 +0530

    Update ChangePassword.js

--- components/MultiVendor/My Account/ChangePassword.js
+++ components/MultiVendor/My Account/ChangePassword.js
@@ -153,6 +153,7 @@ const ChangePassword = () => {
 												/>
 												<span className='p-viewer1'>
 													<i
+														  tabIndex={0}
 														role="button" 
 														name='oldPasswordEye'
 														onClick={Eye}
