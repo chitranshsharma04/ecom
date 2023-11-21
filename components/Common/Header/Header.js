07bfce078f0afb95693449a45f212174bab2db2c
Chitransh Sharma <mylife.myrule0321@gmail.com>
2023-11-21 14:55:31 +0530

    Bug fixes final

--- components/Common/Header/Header.js
+++ components/Common/Header/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/order */
@@ -17,11 +18,12 @@ import Router from 'next/router';
 import cookie from 'js-cookie';
 import {toast} from 'react-toastify';
 import SpinnerLoader from '@components/Common/SpinnerLoader/SpinnerLoader';
-import { NextImage, Image } from '@components/Common/Image';
 import {useGlobalContext} from '@context/ContextApi';
 import {useCommonState} from '../reducer';
 import NavbarItem from './NavbarItem';
 import Form from 'react-bootstrap/Form';
+import NextImage from '../Image';
+import Image from 'next/image';
 //this is the starting point
 const Header = props => {
 	const {state, dispatch} = useCommonState();
@@ -33,7 +35,7 @@ const Header = props => {
 	const [currencyValue, setCurrencyValue] = React.useState(1);
 	const [headerHeight, setHeaderHeight] = React.useState(0);
 	const [loading, setLoading] = useState(false);
-
+	// console.log(headerHeight,"headerheight")
 	const headerRef = useRef();
 //this is a method to make a callback
 	const onResize = useCallback(() => {
@@ -54,8 +56,8 @@ const Header = props => {
 		} else return {};
 	};
 
-	// let cookiedata = getUserCookie('userAuth');
-	
+	let cookiedata = getUserCookie('userAuth');
+	console.log('cookie', cookiedata);
 	// eslint-disable-next-line no-unused-vars
 	//this is a method to make a callback
 	const getResults = useCallback(async () => {
@@ -89,7 +91,7 @@ const Header = props => {
 				type: 'SET_DATA',
 				data: {
 					...state,
-					resultCategorylist,
+					resultCategorylist : resultCategorylist,
 					searchCategories: items,
 					currencyData: currencyValue,
 				},
@@ -102,7 +104,6 @@ const Header = props => {
 	useEffect(() => {
 		getResults();
 	}, [getResults]);
-
 	// const handleSelect = event => {
 	// 	var index = event.nativeEvent.target.selectedIndex;
 
@@ -197,8 +198,8 @@ const Header = props => {
 			<SpinnerLoader loading={loading} />
 			<div
 				className='header-gap'
-				style={{height: `${parseInt(headerHeight)}0px`}}
-			 />
+				style={{height: parseInt(headerHeight) + 0 + 'px'}}
+			></div>
 			<header className='header' ref={headerRef}>
 				{globalState?.userAuth?.role === 2 ? (
 					''
@@ -259,6 +260,8 @@ const Header = props => {
 											<NextImage
 												src='/assets/images/logo.png'
 												alt='av'
+												width={100}
+												height={100}
 												style={{minHeight: '100rem'}}
 											/>
 											{globalState?.userAuth?.role ===
@@ -283,6 +286,8 @@ const Header = props => {
 											<Image
 												src='/assets/images/logo.png'
 												alt='av'
+												width={200}
+												height={100}
 											/>
 											{/* {globalState?.userAuth?.role === 2 ? (
 										<span className='text'>DS E-Commerce</span>
@@ -356,7 +361,7 @@ const Header = props => {
 											{!isAuthenticated ? (
 												<a
 													className='heart-icon'
-													href='#top'
+													href='#'
 													onClick={() =>
 														toast.info(
 															'Please login to View Wishlist',
@@ -392,13 +397,13 @@ const Header = props => {
 											<Link href='/vendor/dashboard'>
 												&nbsp;
 												<>
-													<Image
-														src={`${globalState?.userAuth?.profile_pic}`}
+													{globalState?.userAuth?.profile_pic && <NextImage
+														src={globalState?.userAuth?.profile_pic}
 														width={50}
 														height={50}
 														alt='profile'
 														className='user-img mr-3 profile-img'
-													/>
+													/>}
 													{globalState?.userAuth
 														?.firstname
 														? `${globalState?.userAuth?.firstname} ${globalState?.userAuth?.lastname}`
@@ -413,8 +418,8 @@ const Header = props => {
 												{' '}
 												&nbsp;
 												<>
-													<Image
-														src={`${globalState?.userAuth?.profile_pic}`}
+													<NextImage
+														src={globalState?.userAuth?.profile_pic}
 														width={50}
 														height={50}
 														alt='profile'
